9a0dc674ddb89bebdb7a87230fc7d9a438a2a11d
Tom Maple <tom.maple@outlook.com>
2024-09-04 21:57:07 -0400

    feat: Added the `MessageTemplate` record (#4).

---
+++ src/Result/MessageTemplate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Maple.Result;
+
+/// <summary>
+///     The structure for a templated message.
+/// </summary>
+/// <param name="TemplateId">The identifier of the template.</param>
+/// <param name="Params">
+///     The optional collection of parameters (name and value) that might be required to
+///     generate a message from the specific template.
+/// </param>
+public readonly record struct MessageTemplate(string TemplateId, IReadOnlyDictionary<string, object>? Params = null);
