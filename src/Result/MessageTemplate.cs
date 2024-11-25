44142352c6b4660aab0a172089abd6f9e1ced1a4
Tom Maple <tom.maple@outlook.com>
2024-11-24 22:44:21 -0500

    feat: Updated XML-docs of the `MessageTemplate` structure (#7).

--- src/Result/MessageTemplate.cs
+++ src/Result/MessageTemplate.cs
@@ -5,9 +5,12 @@ namespace Maple.Result;
 /// <summary>
 ///     The structure for a templated message.
 /// </summary>
-/// <param name="TemplateId">The identifier of the template.</param>
+/// <remarks>
+///     The purpose of this structure is to provide a way to generate client-side localized messages.
+/// </remarks>
+/// <param name="TemplateId">The identifier of the message template.</param>
 /// <param name="Params">
-///     The optional collection of parameters (name and value) that might be required to
+///     The optional collection of parameters (names and values) that might be required to
 ///     generate a message from the specific template.
 /// </param>
 public readonly record struct MessageTemplate(string TemplateId, IReadOnlyDictionary<string, object>? Params = null);
