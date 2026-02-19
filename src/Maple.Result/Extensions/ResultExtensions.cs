eab4709b728e67bccb98518ab9d915ec41b89af7
Tom Maple <tom.maple@outlook.com>
2026-02-18 21:46:00 -0500

    feat: Added `.ToResult()` extension method.

---
+++ src/Maple.Result/Extensions/ResultExtensions.cs
@@ -0,0 +1,40 @@
+﻿// SPDX-License-Identifier: MIT
+/*
+ * This code is a part of a Maple.Result library project.
+ * https://github.com/TomMaple/Result/
+ * Copyright (c) Tom Maple
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+using System;
+
+namespace Maple.Result.Extensions;
+
+/// <summary>
+///     The collection of extension methods for the <see cref="Result{T}" /> type.
+/// </summary>
+public static class ResultExtensions
+{
+    /// <summary>
+    ///     Converts the generic <see cref="Result{T}" /> to the non-generic <see cref="Result" />.
+    /// </summary>
+    /// <typeparam name="T">The type of the value contained in the <see cref="Result{T}" />.</typeparam>
+    /// <param name="result">The <see cref="Result{T}" /> instance to convert.</param>
+    /// <returns>
+    ///     A non-generic <see cref="Result" /> representing a success state or
+    ///     a failure state (with the original <see cref="Error" />).
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    ///     If the <paramref name="result" /> is <see langword="null" />.
+    /// </exception>
+    public static Result ToResult<T>(this Result<T> result)
+    {
+        ArgumentNullException.ThrowIfNull(result);
+
+        return result.IsSuccess()
+            ? Result.Success()
+            : Result.FromError(result.Error!);
+    }
+}
---
+++ tests/Maple.Result.Tests.Unit/Extensions/ResultExtensionsUnitTests.cs
@@ -0,0 +1,109 @@
+﻿// SPDX-License-Identifier: MIT
+/*
+ * This code is a part of a Maple.Result library project.
+ * https://github.com/TomMaple/Result/
+ * Copyright (c) Tom Maple
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+using System;
+using Maple.Result.Extensions;
+
+namespace Maple.Result.Tests.Unit.Extensions;
+
+public class ResultExtensionsUnitTests
+{
+    [Fact]
+    public void ToResult_NoGenericResult_ThrowsException()
+    {
+        // Arrange
+        const Result<int>? Sut = null;
+
+        // Act
+        var exception = Record.Exception(() => Sut!.ToResult());
+
+        // Assert
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<ArgumentNullException>();
+        exception.Message.ShouldStartWith("Value cannot be null.");
+    }
+
+    [Fact]
+    public void ToResult_SuccessfulGenericResult_ReturnsSuccessfulResult()
+    {
+        // Arrange
+        const int InitialValue = 362;
+
+        Result<int> sut = InitialValue;
+
+        // Act
+        var result = sut.ToResult();
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.IsSuccess().ShouldBeTrue();
+        result.ShouldBeOfType<Result>();
+    }
+
+    [Fact]
+    public void ToResult_SuccessfulGenericResult_DoesNotModifyGenericResult()
+    {
+        // Arrange
+        const int InitialValue = 362;
+
+        Result<int> sut = InitialValue;
+
+        // Act
+        _ = sut.ToResult();
+
+        // Assert
+        sut.ShouldNotBeNull();
+        sut.IsSuccess().ShouldBeTrue();
+        sut.Value.ShouldBe(InitialValue);
+    }
+
+    [Fact]
+    public void ToResult_ErrorGenericResult_ReturnsErrorResultWithOriginalError()
+    {
+        // Arrange
+        var error = GetError();
+
+        Result<int> sut = error;
+
+        // Act
+        var result = sut.ToResult();
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.IsSuccess().ShouldBeFalse();
+        result.Error.ShouldBe(error);
+    }
+
+    [Fact]
+    public void ToResult_ErrorGenericResult_DoesNotModifyGenericResult()
+    {
+        // Arrange
+        var error = GetError();
+
+        Result<int> sut = error;
+
+        // Act
+        _ = sut.ToResult();
+
+        // Assert
+        sut.ShouldNotBeNull();
+        sut.IsSuccess().ShouldBeFalse();
+        sut.Error.ShouldBe(error);
+    }
+
+    #region helper methods
+
+    private static Error GetError()
+    {
+        return Error.Failure(ErrorUri.None(), "4b3612e7-d22a-45f2-9a5c-4a8c14d39141", "Error title 1");
+    }
+
+    #endregion
+}
